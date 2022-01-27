e22e88edd9105055d8f0cbc2e73535380689ba08
siddharth <93536983+siddfirepro@users.noreply.github.com>
2022-01-27 07:17:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song="";
+function preload(){
+    song=loadSound("BTS.mp3");
+    song=loadSound("ocean blue.mp3");
+}
+function setup(){
+    canvas= createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    }
+    function draw(){
+        image(video,0,0,600,500);
+    }
+    function play(){
+        song.play();
+    }
